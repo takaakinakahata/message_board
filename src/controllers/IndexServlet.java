f4c922a5c8887eb266fe74223a686eb879798c10
takaaki.nakahata <nataka0731@gmail.com>
2023-07-09 17:57:13 +0900

    バリデーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -18,7 +18,7 @@ import utils.DBUtil;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet("/Index")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
